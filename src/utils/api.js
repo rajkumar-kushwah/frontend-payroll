9bdd1cfceebf703f8909deab0a70e81cd80b69ec
rajkumar kushwah <rajkumarkushwah3262@gmail.com>
2025-09-16 16:25:55 +0530

    frontend-update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,18 @@
-// client/utils/api.js
-import axios from 'axios';
+  // client/utils/api.js
+  import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'https://vercle-backend-payroll.vercel.app/api', // Adjust if your server runs elsewhere
-     headers: { "Content-Type": "application/json" },
-});
+  console.log(" VITE API URL:", import.meta.env.VITE_API_URL);
 
-//(Optional) Agar toen stor karna hai:
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if(token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-})
+  const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL, // Adjust if your server runs elsewhere
+      headers: { "Content-Type": "application/json" },
+  });
 
-export default api;
+  //(Optional) Agar toen stor karna hai:
+  api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if(token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  })
+
+  export default api;
