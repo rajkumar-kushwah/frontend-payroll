a2b6854ce384f5855dd1650328e0615cea400c0b
rajkumar kushwah <rajkumarkushwah3262@gmail.com>
2025-12-19 17:24:37 +0530

    update leavfiler

--- src/leaves/LeaveFilter.jsx
+++ src/leaves/LeaveFilter.jsx
@@ -4,7 +4,7 @@ const LeaveFilter = ({ filter, setFilter }) => {
   return (
     <div className="flex text-xs items-center gap-2">
       <h4 className="font-semibold text-sm mr-2">Status:</h4>
-      {["Pending", "Approved", "Rejected"].map((s) => (
+      {["pending", "approved", "rejected"].map((s) => (
         <button
           key={s}
           onClick={() => setFilter(s)}
@@ -12,7 +12,7 @@ const LeaveFilter = ({ filter, setFilter }) => {
             filter === s ? "bg-blue-500 text-white" : "bg-white text-gray-700 border-gray-300"
           }`}
         >
-          {s.charAt(0).toUpperCase() + s.slice(1).toLowerCase()}
+          {s.toUpperCase()}
         </button>
       ))}
     </div>
