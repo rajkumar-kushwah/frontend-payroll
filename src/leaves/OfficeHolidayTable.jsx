8ff6164ebe0713b0c86c3d671f95e114b349a417
rajkumar kushwah <rajkumarkushwah3262@gmail.com>
2025-12-23 15:14:41 +0530

    udate

--- src/leaves/OfficeHolidayTable.jsx
+++ src/leaves/OfficeHolidayTable.jsx
@@ -14,10 +14,10 @@ const OfficeHolidayTable = ({ holidays = [], user, onDelete }) => {
         <thead className="bg-gray-100">
           <tr>
             <th className="p-2 text-left border-b">Title</th>
-            <th className="p-2 border-b">Date</th>
-            <th className="p-2 border-b">Type</th>
-            <th className="p-2 border-b">Description</th>
-            {canDelete && <th className="p-2 border-b">Action</th>}
+            <th className="p-2 text-left border-b">Date</th>
+            <th className="p-2 text-left border-b">Type</th>
+            <th className="p-2 text-left border-b">Description</th>
+            {canDelete && <th className="p-2 text-left border-b">Action</th>}
           </tr>
         </thead>
 
@@ -34,18 +34,18 @@ const OfficeHolidayTable = ({ holidays = [], user, onDelete }) => {
           ) : (
             holidays.map((h) => (
               <tr key={h._id} className="border-t">
-                <td className="p-2 border-b">{h.title}</td>
-                <td className="p-2 border-b">
+                <td className="p-2 text-left border-b">{h.title}</td>
+                <td className="p-2 text-left border-b">
                   {new Date(h.date).toLocaleDateString()}
                 </td>
-                <td className="p-2 border-b capitalize">{h.type}</td>
-                <td className="p-2 border-b">{h.description || "-"}</td>
+                <td className="p-2 text-left border-b capitalize">{h.type}</td>
+                <td className="p-2  text-left border-b">{h.description || "-"}</td>
 
                 {canDelete && (
-                  <td className="p-2 border-b">
+                  <td className="p-2 text-left border-b">
                     <button
                       onClick={() => onDelete(h._id)}
-                      className="px-2 py-1 bg-red-500 hover:bg-red-600 cursor-pointer text-xs text-white rounded"
+                      className="px-2 py-1 text-left bg-red-500 hover:bg-red-600 cursor-pointer text-xs text-white rounded"
                     >
                       Delete
                     </button>
